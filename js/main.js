1a5cb75d3e8b13a6759892d38043f53e23e00945
LorenzoMoccia <moccialorenzo98@gmail.com>
2022-10-20 13:20:24 +0200

    ARRIVATI A DESTINAZIONE, STRANAMENTE IN ANTICIPO

--- js/main.js
+++ js/main.js
@@ -6,3 +6,15 @@ const userKm = Number(prompt("Quanti km vuoi percorrere?"));
 
 let ticketPrice = 0.21 * userKm;
 
+if(userAge < 18){
+    let priceUnderAge = ticketPrice - (ticketPrice * 0.2);
+    console.log("Sei minorenne quindi il tuo prezzo è: " + priceUnderAge);
+}
+else if(userAge >= 65)
+{
+    let priceOverAge = ticketPrice - (ticketPrice * 0.4);
+    console.log("Sei un vecchiaccio pensionato, il tuo prezzo è: " + priceOverAge);
+}
+else{
+    console.log("Il prezzo del tuo biglietto e': " + ticketPrice);
+}
