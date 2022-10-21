be2778b90235f6f130c42dc82a12a325f747c2ef
LorenzoMoccia <moccialorenzo98@gmail.com>
2022-10-21 09:18:22 +0200

    fix decimali

--- js/main.js
+++ js/main.js
@@ -8,13 +8,13 @@ let ticketPrice = 0.21 * userKm;
 
 if(userAge < 18){
     let priceUnderAge = ticketPrice - (ticketPrice * 0.2);
-    console.log("Sei minorenne quindi il tuo prezzo è: " + priceUnderAge);
+    console.log("Sei minorenne quindi il tuo prezzo è: " + priceUnderAge.toFixed(2));
 }
 else if(userAge >= 65)
 {
     let priceOverAge = ticketPrice - (ticketPrice * 0.4);
-    console.log("Sei un vecchiaccio pensionato, il tuo prezzo è: " + priceOverAge);
+    console.log("Sei un vecchiaccio pensionato, il tuo prezzo è: " + priceOverAge.toFixed(2));
 }
 else{
-    console.log("Il prezzo del tuo biglietto e': " + ticketPrice);
+    console.log("Il prezzo del tuo biglietto e': " + ticketPrice.toFixed(2));
 }
